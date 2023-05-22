16e797156bae7cb92c98970c39ce1a0dadf6e13c
cuong.nv <cuong.vn@evotek.vn>
2023-05-22 11:43:26 +0700

    commit at 11h43

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { CommonService } from '../Services/common.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public name = 'Huy Nguyễn';
+  public age: any;
+  public vehicles = ['Toyota', 'Honda', 'Nissan', 'Ford', 'Mustang'];
 
+  // constructor(private common: CommonService) {
+  //   // this.age = common.age;
+  // }
+
+  ngOnInit(): void {}
+
+  public tangTuoi() {
+    // this.common.age++;
+    // this.age = this.common.age;
+    // if (this.age === 20) {
+    //   this.name = 'Hahaha';
+    // }
+    // this.vehicles.push(this.name + ': ' + this.age);
+  }
 }
